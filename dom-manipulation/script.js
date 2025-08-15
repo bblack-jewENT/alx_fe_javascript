cdcde919b8b567d321a55e0bda298467d019826c
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-08-15 23:00:43 +0200

    Update script.js
    Append a function to display a random quote.

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,3 +4,11 @@ let quotes = [
     { text: "Life is what happens when you're busy making other plans.", category: "Life" },
     { text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.", category: "Individuality" }
 ];
+
+// 2. Function to display a random quote
+function showRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    const display = document.getElementById('quoteDisplay');
+    display.innerHTML = `<p>"${quote.text}"</p><small>Category: ${quote.category}</small>`;
+}
