64a93c880e58084b2587e3dbbb6d3d2021ea35ba
Moses Rametsi <158572657+bblack-jewENT@users.noreply.github.com>
2025-08-15 22:59:26 +0200

    Update script.js
    Append an array of objects containing quotes.

---
+++ dom-manipulation/script.js
@@ -0,0 +1,6 @@
+// 1. Array to store quote objects
+let quotes = [
+    { text: "The only limit to our realization of tomorrow is our doubts of today.", category: "Motivation" },
+    { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+    { text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.", category: "Individuality" }
+];
